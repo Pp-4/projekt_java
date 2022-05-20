5f4cec89a0bb49912e00b4060809d0c9d86d2d8a
AJ-pP4 <39858427+Pp-4@users.noreply.github.com>
2022-05-21 00:49:05 +0200

    testy czytania bazy,na razie nie działa(średniki , nie przecinki)

--- ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/ReadFromFile.java
+++ ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/ReadFromFile.java
@@ -1,6 +1,51 @@
 package com.company.CommonClasses.CrudActions;
 
+import java.io.*;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ReadFromFile {
-    
+    public class graPlanszowa {//java nie posiada typu uint
+        int Id;
+        String Name;
+        int YearPublished;
+        int MinPlayers;
+        int MaxPlayers;
+        int PlayTime;
+        int MinAge;
+        int UsersRated;
+        int RatingAverage;
+        int ComplexityAverage;
+        int OwnedUsers;
+        int Mechanics;
+        int Domains;
+        ArrayList<String> Tags;
+        public graPlanszowa(int a,String b,int c,int d,int e,int f,int g,int h,int i,int j,int k,int l,int m,ArrayList<String> n){
+            Id = a;Name = b;YearPublished = c;MinPlayers = d;MaxPlayers = e;PlayTime = f;MinAge = g;UsersRated = h;RatingAverage = i;ComplexityAverage = j;OwnedUsers = k;Mechanics = l;Domains = m;Tags = n;
+        }
+    }
+  //TO DO
+  //odczyt plików csv
+  //zapis do pliku csv
+  //serializacja/deserializacja obiektów/csv
+  public ArrayList <graPlanszowa> read(String nazwaBazy) throws Exception { //ta metoda odczytuje całą bazę z pliku i zwraca tablicę obiektów utworzonych z tej bazy
+    //zakładam ,że program zostanie uruchomiony na windowsie oraz ,że baza lokalna znajduje się na pulpicie
+    File pulpit = new File(System.getProperty("user.home"), "/Desktop");
+    if(!new File(pulpit, nazwaBazy).canRead())throw new FileNotFoundException("nie udało się odczytać pliku z bazą");  
+    Scanner bazaDanych = new Scanner(new File(pulpit, nazwaBazy));
+    bazaDanych.useDelimiter(","); //plik csv zakłada przecinki do odzielania wartości
+    ArrayList <graPlanszowa> listaGier = new ArrayList<graPlanszowa>();
+    bazaDanych.next();//pomijam linijkę 0, w której są nazwy kolumn
+    while (bazaDanych.hasNext()) {
+        String[] temp = bazaDanych.next().split(",");
+        int length = temp.length;
+        ArrayList <String> tagi = new ArrayList<String>();//w każdej lini występuje minimum 14 parametrów, licząc od 1,14 i kolejne parametry muszą zostać umieszczone w tablicy tagów
+        if(length>=14)for(int i = 13;i<length;i++)tagi.add(temp[i]);
+        //god help me , co ja zrobiłem, na pewno da się lepiej
+        graPlanszowa tempGame = new graPlanszowa(Integer.parseInt(temp[0]), temp[1], Integer.parseInt(temp[2]), Integer.parseInt(temp[3]), Integer.parseInt(temp[4]), Integer.parseInt(temp[5]), Integer.parseInt(temp[6]), Integer.parseInt(temp[7]), Integer.parseInt(temp[8]), Integer.parseInt(temp[9]), Integer.parseInt(temp[10]), Integer.parseInt(temp[11]), Integer.parseInt(temp[12]), tagi);
+        listaGier.add(tempGame);
+    }
+    bazaDanych.close();
+    return listaGier;
+  }
 }
--- ProjektBiblioteka/src/com/company/Main.java
+++ ProjektBiblioteka/src/com/company/Main.java
@@ -1,12 +1,18 @@
 package com.company;
 
 import com.company.CommonClasses.BoardGameRental;
-
+import com.company.CommonClasses.CrudActions.ReadFromFile;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BoardGameRental boardGameRental = new BoardGameRental();
+        //BoardGameRental boardGameRental = new BoardGameRental();
+        ReadFromFile test = new ReadFromFile();
+        try {//test importu z pliku
+            test.read("bgg_dataset.csv");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
