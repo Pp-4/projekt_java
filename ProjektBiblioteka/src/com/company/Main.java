c976113030677a41bd465c9031eecf174fd9ac78
AJ-pP4 <39858427+Pp-4@users.noreply.github.com>
2022-05-21 19:56:26 +0200

    już nie odczytuje co 2 linijki , zawiecha na lini 653

--- ProjektBiblioteka/src/com/company/Main.java
+++ ProjektBiblioteka/src/com/company/Main.java
@@ -7,16 +7,14 @@ import com.company.CommonClasses.CrudActions.ReadFromFile;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static StackTraceElement[] stackTrace;
+
+    public static void main(String[] args) throws Exception {
 
         //BoardGameRental boardGameRental = new BoardGameRental();
         ReadFromFile test = new ReadFromFile();
-        try {//test importu z pliku
+
             ArrayList test2 =  test.read("bgg_dataset.csv");
             test2.size();
-        } catch (Exception e) {
-            e.printStackTrace();
-            e.getCause();
-        }
     }
 }
