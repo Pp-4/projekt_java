2a460801a044667554e40c6fbb7d25895c3f0362
AJ-pP4 <39858427+Pp-4@users.noreply.github.com>
2022-05-28 11:34:01 +0200

    test.read obsługuje odczyt i dopasowanie do modelu
    Signed-off-by: AJ-pP4 <39858427+Pp-4@users.noreply.github.com>

---
+++ ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/FilterTable.java
@@ -0,0 +1,8 @@
+package com.company.CommonClasses.CrudActions;
+
+public abstract class FilterTable {
+    //TODO Filtrowanie tabeli
+    public static void fitruj(){
+
+    }
+}
--- ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/ReadFromFile.java
+++ ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/ReadFromFile.java
@@ -1,69 +1,58 @@
 package com.company.CommonClasses.CrudActions;
-
 import java.io.*;
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Scanner;
-import javax.naming.directory.InvalidAttributeIdentifierException;
 import java.util.Locale;
+import java.util.Scanner;
+import java.util.ArrayList;
 import java.text.NumberFormat;
-//TODO:ZAPIS , SERIALIZACJA?
-public class ReadFromFile {//obsługa tabeli
-  public class graPlanszowa {
-    // wszytie typy muszą mieć wrappery(typy pierwotne nie mają metody/pola class , dlaczego ? nie wiem)
-    Integer Id;
-    String Name;
-    Integer YearPublished;
-    Integer MinPlayers;
-    Integer MaxPlayers;
-    Integer PlayTime;
-    Integer MinAge;
-    Integer UsersRated;
-    Double RatingAverage;
-    Integer BGGRank;
-    Double ComplexityAverage;
-    Integer OwnedUsers;
-    String[] Mechanics;
-    String[] Domains;
-    public graPlanszowa() {}
-  }
+import java.lang.reflect.Field;
+import com.company.CustomErrors.WrongTypeException;
 
+public class ReadFromFile {//odczyt z tabeli lokalnej
 
-  public ArrayList<graPlanszowa> read(String nazwaBazy) throws Exception { //ta metoda odczytuje całą tabelę z pliku i zwraca tablicę obiektów utworzonych z tej tabeli
-    //na potrzeby testu zakładam ,że program zostanie uruchomiony na windowsie oraz ,że tabela lokalna znajduje się na pulpicie
-    File pulpit = new File(System.getProperty("user.home"), "/Desktop");
-    if (!new File(pulpit, nazwaBazy).canRead()) throw new FileNotFoundException("nie udało się odczytać pliku z bazą");
-
-    Scanner bazaDanych = new Scanner(new File(pulpit, nazwaBazy),"UTF-8");//wymagana informacja o kodowaniu, bez niej scanner nie działa poprawnie
-    ArrayList<graPlanszowa> listaGier = new ArrayList<graPlanszowa>();
+  public <T> ArrayList<T> read(String nazwaBazy, String lokalizacja ,Class<T> model) throws Exception {
+    //ta metoda odczytuje całą tabelę z pliku i zwraca tablicę obiektów utworzonych z tej tabeli
+    Scanner bazaDanych = uchwyt(nazwaBazy, lokalizacja);
+    ArrayList<T> listaGier = new ArrayList<T>();
     bazaDanych.useDelimiter("\n|\\;");//tekst będzie rozbity przez średnik lub znak nowej lini
-    bazaDanych.nextLine(); //pierwszy wiersz zaiwera nazwy kolumn
-    int a = 0;  
+    bazaDanych.nextLine(); //pierwszy wiersz zawiera nazwy kolumn
     while (bazaDanych.hasNext()) { //zapis kolejnych wierszy w tabeli do listy obiektów
-  
-      graPlanszowa tempGame = new graPlanszowa();
+      T tempGame = model.getConstructor().newInstance();//MAGIA
       Field[] pola = tempGame.getClass().getDeclaredFields();
       String temp ="";//deklaracja zmiennej poza pętlą for ponieważ dla pola this$0 nie używam .next() (patrz niżej) 
       for (Field f : pola) {//pętla przypisuje do tempgame kolejne znalezione w tabeli wartości
         Class t = f.getType();//sprawdza typ aby wiedzieć jak sformatować otrzymanego stringa
-        //edge case ,w przypadku klasy wewnętrznej do listy pól zostaje dodany parametr this$0 ,który trzeba olać
-          if(t.getName() != this.getClass().getName())temp = bazaDanych.next();//dla pola this$0 nie odczytujemy wartości  
-          if(a>20340)System.out.println(t.getName()+" "+f.getName()+" "+temp);//testy
+          if(t.getName() != this.getClass().getName() && f.getName()!= "isAvailable")temp = bazaDanych.next();//dla pola this$0 nie odczytujemy wartości , pole isAvailable pomijamy
+          //System.out.println(t.getName()+" "+f.getName()+" "+temp);//testy
           if (t == Integer.class) f.set(tempGame, TryParseStringToInteger(temp));
-          else if (t.getName() == this.getClass().getName());//edge case ,w przypadku inner class do listy pól zostaje dodany parametr this$0 ,który trzeba olać
           else if (t == Double.class) f.set(tempGame, TryParseStringToDouble(temp));
-          else if (t == String[].class) f.set(tempGame, temp.split(","));
+          else if (t == String[].class) f.set(tempGame, temp.trim().split(","));
           else if (t == String.class) f.set(tempGame, temp);
-          else throw new InvalidAttributeIdentifierException("Nieprawidłowy typ zmiennej!,\n Nie wiem jaki dać wyjątek #TODO");
+          else if (t == Boolean.class) f.set(tempGame, true);
+          else if (t.getName() == this.getClass().getName());
+          //edge case ,w przypadku inner class do listy pól zostaje dodany parametr this$0 ,który trzeba olać
+          else throw new WrongTypeException();
       }
       listaGier.add(tempGame);
-      if(a%20342==0 && a!=0){
-        System.out.println(a);//testy
-      }
-        a++;
+      /*if(listaGier.size()>18985 && listaGier.size()%100 == 0){
+        System.out.println(listaGier.size());//debugowanie
+      }*/
     }
     bazaDanych.close();
     return listaGier;
+  }
+ Scanner uchwyt(String plik,String lokalizacja) throws FileNotFoundException{
+    Scanner bazaDanych;
+    //wymagana informacja o kodowaniu, bez niej scanner nie działa poprawnie
+    try {//próba znaliezienia pliku w podanej lokalizacji
+      bazaDanych = new Scanner(new File(lokalizacja, plik),"UTF-8");
+    } catch (Exception e) {
+      try {//próba znaleziienia pliku na pulpicie
+        bazaDanych = new Scanner(new File(new File(System.getProperty("user.home"), "/Desktop"), plik),"UTF-8");
+      } catch (Exception f) {
+        throw new FileNotFoundException("nie udało się odczytać pliku z bazą");
+      }
+    }
+    return bazaDanych;
   }
  Integer TryParseStringToInteger(String x){//obsługa konwersji stringa na int
   Integer y; 
