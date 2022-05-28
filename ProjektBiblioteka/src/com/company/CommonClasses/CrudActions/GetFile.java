6b4ddde83e38be3195a3966120b8ce46715bd11b
AJ-pP4 <39858427+Pp-4@users.noreply.github.com>
2022-05-28 14:59:50 +0200

    zapis prawie działa (zostały tablice z tagami)
    Signed-off-by: AJ-pP4 <39858427+Pp-4@users.noreply.github.com>

--- ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/GetFile.java
+++ ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/GetFile.java
@@ -4,16 +4,31 @@ import java.util.Scanner;
 public class GetFile {
     static Scanner uchwyt(String plik,String lokalizacja) throws FileNotFoundException{
         Scanner bazaDanych;
-        //wymagana informacja o kodowaniu, bez niej scanner nie działa poprawnie
-        try {//próba znaliezienia pliku w podanej lokalizacji
-          bazaDanych = new Scanner(new File(lokalizacja, plik),"UTF-8");
+        File temp = Plik(plik, lokalizacja);
+        if(temp == null)throw new FileNotFoundException("nie udało się odczytać pliku z tabelą");
+        try {
+          bazaDanych = new Scanner(temp,"UTF-8"); //wymagana informacja o kodowaniu, bez niej scanner nie działa poprawnie
         } catch (Exception e) {
-          try {//próba znaleziienia pliku na pulpicie
-            bazaDanych = new Scanner(new File(new File(System.getProperty("user.home"), "/Desktop"), plik),"UTF-8");
-          } catch (Exception f) {
-            throw new FileNotFoundException("nie udało się odczytać pliku z tabelą");
-          }
+          throw new FileNotFoundException();
         }
         return bazaDanych;
+      }
+      public static File Plik(String plik,String lokalizacja){
+        File uchwyt;
+        try {
+          uchwyt = new File(lokalizacja, plik);
+        }
+        catch (Exception e) 
+        {
+          try
+          {
+            uchwyt = new File(new File(System.getProperty("user.home"), "/Desktop"), plik);
+          }
+          catch (Exception f) 
+          {
+            uchwyt = null;
+          }
+        }
+        return uchwyt;
       }
 }
--- ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/WriteToFile.java
+++ ProjektBiblioteka/src/com/company/CommonClasses/CrudActions/WriteToFile.java
@@ -1,5 +1,39 @@
 package com.company.CommonClasses.CrudActions;
-
+import java.io.*;
+import java.lang.reflect.*;
+import java.util.ArrayList;
 public class WriteToFile {
-    //TODO zapis do tabeli
+    public <T> Boolean write(String nazwaBazy, String lokalizacja ,ArrayList<T> tabela) {
+        File plik = GetFile.Plik(nazwaBazy, lokalizacja);
+        if(plik == null)return false;//gdyby nie udało znaleźć się pliku docelowego
+        try {
+            BufferedWriter zapis = null;
+            zapis = new BufferedWriter(new FileWriter(plik));
+
+            //zapisanie nazw kolumn na początku tabeli
+            Field[] wiersz0 = tabela.get(0).getClass().getDeclaredFields();
+            String nazwyKolumn = "";
+            for (Field pole : wiersz0) {
+                nazwyKolumn+=pole.getName().toString()+";";
+            }
+            zapis.write(nazwyKolumn+"\n");
+
+            //zapisywanie kolejnych wierszy w tabeli
+            for (T wiersz : tabela) {
+                Field[] pola = wiersz.getClass().getDeclaredFields();
+                String temp = "";
+                for (Field pole : pola) {
+                    temp+=pole.get(wiersz).toString()+";";
+                }
+                zapis.write(temp+"\n");
+            }
+            zapis.flush();
+            zapis.close();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Błąd przy zapisie pliku.");
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
