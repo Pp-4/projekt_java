47466bd6b9c9e3fff39c6027f4dd5b0ad6d504b4
Miszui <kamillo393@gmail.com>
2022-06-05 16:36:34 +0200

    Dodanie podpowiedzi w CommonMessages

--- ProjektBiblioteka/src/com/company/CommonClasses/Messages/CommonMessages.java
+++ ProjektBiblioteka/src/com/company/CommonClasses/Messages/CommonMessages.java
@@ -6,10 +6,43 @@ public  class CommonMessages {
 
 
 
-    public static void LogInInstrucitions(){
-        //TODO: wyświetlnie opcji wyoru przy logowaniu
-        //podobnie przy innych stanach
+    public static void MenuInstrucition(){
+        //Podpowiedź gdy niezalogowano użytkownika
+        ClearConsole.clearConsole();
+        System.out.println("-------------------");
+        System.out.println("L - Log in");
+        System.out.println("R - Register new user");
+        System.out.println("U - Show available users");
+        System.out.println("B - Show available board game ");
+        System.out.println("Q - Exit");
+        System.out.println("-------------------");
+
     }
+    public static void AdminInstrucition(){
+        //Podpowiedź gdy zalogowano jako administrator
+        ClearConsole.clearConsole();
+        System.out.println("-------------------");
+        System.out.println("L - Log out");
+        System.out.println("I - Show your data");
+        System.out.println("U - Show and edit available users");
+        System.out.println("B - Show and edit available board game ");
+        System.out.println("Q - Exit");
+        System.out.println("-------------------");
+
+    }
+    public static void UserInstrucition(){
+        //Podpowiedź gdy zalogowano jako użytkownik
+        ClearConsole.clearConsole();
+        System.out.println("-------------------");
+        System.out.println("L - Log out");
+        System.out.println("I - Show your data");
+        System.out.println("U - Show available users");
+        System.out.println("B - Show available board game ");
+        System.out.println("Q - Exit");
+        System.out.println("-------------------");
+
+    }
+
 
 
     public static void ShowInstruction(){
@@ -22,5 +55,10 @@ public  class CommonMessages {
         System.out.println("Q - Exit");
         System.out.println("-------------------");
     }
+
+
+
+
+
 
 }
