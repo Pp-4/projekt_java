7f37959293dbdf2fc9ee7e1eeb41645b0a5f5f03
JakubWachowicz <wachowiczkuba23@gmail.ocm>
2022-06-07 20:13:51 +0200

    Skromna poprawka FileLocations

--- ProjektBiblioteka/src/com/company/OtherFiles/FileLoactions.java
+++ ProjektBiblioteka/src/com/company/OtherFiles/FileLoactions.java
@@ -6,7 +6,7 @@ public class FileLoactions {
     public static List<String> Tablice(){
         List<String> lokalizacje = new ArrayList<String>();
         lokalizacje.add("");
-        lokalizacje.add("\\ProjektBiblioteka\\src\\com\\company\\OtherFiles");
+        lokalizacje.add("\\projekt_java\\ProjektBiblioteka\\src\\com\\company\\OtherFiles");
         lokalizacje.add("\\src\\com\\company\\OtherFiles");
         return lokalizacje;
     }
