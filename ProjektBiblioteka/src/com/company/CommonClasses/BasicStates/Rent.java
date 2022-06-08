1c31990ab42676b8b30fc4e356b106accabc6470
AJ-pP4 <39858427+Pp-4@users.noreply.github.com>
2022-06-08 13:06:21 +0200

    rozpoczęcie prac nad wypożyczaniem
    Signed-off-by: AJ-pP4 <39858427+Pp-4@users.noreply.github.com>

--- ProjektBiblioteka/src/com/company/CommonClasses/BasicStates/Rent.java
+++ ProjektBiblioteka/src/com/company/CommonClasses/BasicStates/Rent.java
@@ -1,7 +1,18 @@
 package com.company.CommonClasses.BasicStates;
 
+import java.util.ArrayList;
+
+import com.company.CommonClasses.CrudActions.ReadFromFile;
+import com.company.Models.BoardGame;
+import com.company.Models.Rental;
+import com.company.Models.User;
+
 public class Rent {
-    public Rent(){
+    public Rent()throws Exception{
         System.out.println("Chwilowo nie da sie wyporzyczac");
+        ReadFromFile test = new ReadFromFile();//przykładowy odczyt tabeli i zapisanie wyników do arraylist
+        ArrayList<BoardGame> ListaGier =  test.read("bgg_dataset.csv",BoardGame.class);
+        ArrayList<User> Listaużytkowników =  test.read("bgg_dataset.csv",User.class);
+        ArrayList<Rental> ListaWypozyczen =  test.read("bgg_dataset.csv",Rental.class);
     }
 }
